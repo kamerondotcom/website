f6700835abb47691ce5ac0f18aef8af0fd15e236
Kameron Zach <kameron.zach@equinox.com>
2019-05-03 15:05:09 -0700

    Tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import Div100vh from "react-div-100vh";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
@@ -10,14 +10,14 @@ import "./../scss/home.scss";
 const IndexPage = () => (
   <Layout>
     <SEO title="Kameron Zach" />
-    <div className="home">
+    <Div100vh className="home">
       <div className="home__main">
         <h1>
           Kameron Zach
           <br />
         </h1>
       </div>
-    </div>
+    </Div100vh>
   </Layout>
 );
 
