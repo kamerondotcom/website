7f80273068aa68a08736250300a2031f7f4fec39
Kameron Zach <kameron.zach@equinox.com>
2020-09-30 15:47:14 -0700

    GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-82620316-3",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
