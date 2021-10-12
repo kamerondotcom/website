99d67fbe97e2a95c8340b8528b5e1e18512c704a
Kameron Zach <kameron@letsbuild.la>
2021-10-11 18:45:26 -0700

    ??

--- src/context/modes.js
+++ src/context/modes.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, useContext } from "react";
+import React, { createContext, useEffect, useReducer, useContext } from "react";
 
 const modeData = {
   isLight: !(new Date().getHours() > 17 && new Date().getHours() > 6),
@@ -10,6 +10,12 @@ export const ModesContext = createContext({});
 
 export const modesReducer = (state, action) => {
   switch (action.type) {
+    case "SET_MODE": {
+      return {
+        ...state,
+        isLight: action.payload,
+      };
+    }
     case "TOGGLE_MODE": {
       return {
         ...state,
@@ -25,6 +31,22 @@ export const modesReducer = (state, action) => {
 
 export const ModesProvider = ({ children }) => {
   const [state, dispatch] = useReducer(modesReducer, modeData);
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_MODE",
+      payload: !(new Date().getHours() > 17 && new Date().getHours() > 6),
+    });
+    setTimeout(() => {
+      //   dispatch({
+      //     type: "SET_MODE",
+      //     payload: !(new Date().getHours() > 17 && new Date().getHours() > 6),
+      //   });
+    }, 100);
+    return () => {
+      // clearInterval(timer);
+    };
+  }, []);
 
   return (
     <ModesContext.Provider value={{ state, dispatch }}>
