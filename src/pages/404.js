31a718474d386ff481795d7bba7453bf86fe322b
Kameron Zach <kameron@letsbuild.la>
2021-11-23 10:18:13 -0800

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,18 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import * as styles from "./../scss/home.module.scss";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.error__main}>
+      <div className={styles.error__header}>
+        <h1>404</h1>
+        <p>The page cannot be found.</p>
+      </div>
+    </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
